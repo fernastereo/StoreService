f2c34d025a03685ab3542e2c8305caff4ad8b999
fernastereo <fernandoecueto@gmail.com>
2020-09-25 19:23:19 -0500

    created AuthorBook class

---
+++ ServiceStore.Api.Author/Model/AcademicGrade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Author.Model
+{
+    public class AcademicGrade
+    {
+    }
+}
---
+++ ServiceStore.Api.Author/Model/AuthorBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Author.Model
+{
+    public class AuthorBook
+    {
+        public int AuthorBookId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+    }
+}
