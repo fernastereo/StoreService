e3d7c190cdd11c1344f81f87c571aeacaf2cc775
fernastereo <fernandoecueto@gmail.com>
2020-09-26 14:15:43 -0500

    implementing CQRS (Command and Query Responsibility Segregation)

---
+++ ServiceStore.Api.Author/Application/New.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Author.Application
+{
+    public class New
+    {
+        public class Execute : IRequest
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public DateTime? BirthDate { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Execute>
+        {
+            public Task<Unit> Handle(Execute request, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
