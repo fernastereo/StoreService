1711fcd1c74ec632639847d6477695fe2e7c0f93
fernastereo <fernandoecueto@gmail.com>
2020-09-28 23:54:18 -0500

    creating model class and setting up EF Core for SQLServer

---
+++ ServiceStore.Api.Book/Persistence/BookContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceStore.Api.Book.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Book.Persistence
+{
+    public class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<BookAuthor> Book { get; set; }
+    }
+}
