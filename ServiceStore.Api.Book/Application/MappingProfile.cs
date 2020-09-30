17085e91ca5b05f490a06113e5c5c17d3edf5ba1
fernastereo <fernandoecueto@gmail.com>
2020-09-30 17:38:23 -0500

    creating application class for queries

---
+++ ServiceStore.Api.Book/Application/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ServiceStore.Api.Book.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Book.Application
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<BookAuthor, BookAuthorDto>();
+        }
+    }
+}
---
+++ ServiceStore.Api.Book/Application/Query.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ServiceStore.Api.Book.Model;
+using ServiceStore.Api.Book.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Book.Application
+{
+    public class Query
+    {
+        public class Execute : IRequest<List<BookAuthorDto>>
+        {
+
+        }
+
+        public class Handler : IRequestHandler<Execute, List<BookAuthorDto>>
+        {
+            private readonly BookContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(BookContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            public async Task<List<BookAuthorDto>> Handle(Execute request, CancellationToken cancellationToken)
+            {
+                var books = await _context.Book.ToListAsync();
+                var booksDto = _mapper.Map<List<BookAuthor>, List<BookAuthorDto>>(books);
+            }
+        }
+    }
+}
