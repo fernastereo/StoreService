81db5f3a297f02f70373972b305e2421ddf47b30
fernastereo <fernandoecueto@gmail.com>
2020-09-29 20:39:12 -0500

    code-first migraations to SqlServer

--- ServiceStore.Api.Book/Model/BookAuthor.cs
+++ ServiceStore.Api.Book/Model/BookAuthor.cs
@@ -7,6 +7,7 @@ namespace ServiceStore.Api.Book.Model
 {
     public class BookAuthor
     {
+        public int BookAuthorId { get; set; }
         public Guid? BookId { get; set; }
         public string Title { get; set; }
         public DateTime? ReleaseDate { get; set; }
--- ServiceStore.Api.Book/Startup.cs
+++ ServiceStore.Api.Book/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ServiceStore.Api.Book.Persistence;
 
 namespace ServiceStore.Api.Book
 {
@@ -25,6 +27,10 @@ namespace ServiceStore.Api.Book
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<BookContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("DBConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
