3eac0322560f468b0bfa8cc85f0cca97eff01bbc
fernastereo <fernandoecueto@gmail.com>
2020-10-01 18:05:11 -0500

    created migrations for mysql database

---
+++ ServiceStore.Api.Shop/Migrations/20201001230255_MySQLInitialMigration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.Data.EntityFrameworkCore.Metadata;
+
+namespace ServiceStore.Api.Shop.Migrations
+{
+    public partial class MySQLInitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShopSession",
+                columns: table => new
+                {
+                    ShopSessionId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopSession", x => x.ShopSessionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShopSessionDetail",
+                columns: table => new
+                {
+                    ShopSessionDetailId = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    SelectedProduct = table.Column<string>(nullable: true),
+                    ShopSessionId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopSessionDetail", x => x.ShopSessionDetailId);
+                    table.ForeignKey(
+                        name: "FK_ShopSessionDetail_ShopSession_ShopSessionId",
+                        column: x => x.ShopSessionId,
+                        principalTable: "ShopSession",
+                        principalColumn: "ShopSessionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopSessionDetail_ShopSessionId",
+                table: "ShopSessionDetail",
+                column: "ShopSessionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShopSessionDetail");
+
+            migrationBuilder.DropTable(
+                name: "ShopSession");
+        }
+    }
+}
--- ServiceStore.Api.Shop/Startup.cs
+++ ServiceStore.Api.Shop/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ServiceStore.Api.Shop.Persistence;
 
 namespace ServiceStore.Api.Shop
 {
@@ -25,6 +27,10 @@ namespace ServiceStore.Api.Shop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<ShopContext>(options =>
+            {
+                options.UseMySQL(Configuration.GetConnectionString("DBConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
