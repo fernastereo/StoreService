0ecaa573243a91369e71770be1bfd179993d5d1b
fernastereo <fernandoecueto@gmail.com>
2020-10-01 17:42:05 -0500

    creating dbcontext for persistence

---
+++ ServiceStore.Api.Shop/Persistence/ShopContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceStore.Api.Shop.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Shop.Persistence
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<ShopSession> ShopSession { get; set; }
+        public DbSet<ShopSessionDetail> ShopSessionDetail { get; set; }
+    }
+}
