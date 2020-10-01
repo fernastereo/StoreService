16d9b2e09c824912794ddb017f3cd9a17a6074ac
fernastereo <fernandoecueto@gmail.com>
2020-10-01 17:38:55 -0500

    creating model for ShopSession

---
+++ ServiceStore.Api.Shop/Model/ShopSession.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Shop.Model
+{
+    public class ShopSession
+    {
+        public int ShopSessionId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public ICollection<ShopSessionDetail> DetailList { get; set; }
+    }
+}
---
+++ ServiceStore.Api.Shop/Model/ShopSessionDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Shop.Model
+{
+    public class ShopSessionDetail
+    {
+        public int ShopSessionDetailId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public string SelectedProduct { get; set; }
+        public int ShopSessionId { get; set; }
+        public ShopSession ShopSession { get; set; }
+    }
+}
