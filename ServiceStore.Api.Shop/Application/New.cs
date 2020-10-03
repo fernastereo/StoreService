e73035d09ff95d23fa5b7b250b576cbfea023469
fernastereo <fernandoecueto@gmail.com>
2020-10-03 12:26:20 -0500

    inserting data to mysql from shop microservice

---
+++ ServiceStore.Api.Shop/Application/New.cs
@@ -0,0 +1,67 @@
+﻿using MediatR;
+using ServiceStore.Api.Shop.Model;
+using ServiceStore.Api.Shop.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Shop.Application
+{
+    public class New
+    {
+        public class Execute : IRequest
+        {
+            public DateTime SessionCreatedAt { get; set; }
+            public List<string> ProductsList { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Execute>
+        {
+            private readonly ShopContext _context;
+
+            public Handler(ShopContext context)
+            {
+                _context = context;
+            }
+            public async  Task<Unit> Handle(Execute request, CancellationToken cancellationToken)
+            {
+                var shopSession = new ShopSession
+                {
+                    CreatedAt = request.SessionCreatedAt
+                };
+                _context.ShopSession.Add(shopSession);
+                var value = await _context.SaveChangesAsync();
+
+                if (value == 0)
+                {
+                    throw new Exception("There was an error on shop session");
+                }
+
+                int id = shopSession.ShopSessionId;
+                foreach (var obj in request.ProductsList)
+                {
+                    var sessionDetail = new ShopSessionDetail
+                    {
+                        CreatedAt = DateTime.Now,
+                        ShopSessionId = id,
+                        SelectedProduct = obj
+                    };
+
+                    _context.ShopSessionDetail.Add(sessionDetail);
+                }
+
+                value = await _context.SaveChangesAsync();
+
+                if (value > 0)
+                {
+                    return Unit.Value;
+                }
+
+                throw new Exception("Products detail could not be saved");
+
+            }
+        }
+    }
+}
