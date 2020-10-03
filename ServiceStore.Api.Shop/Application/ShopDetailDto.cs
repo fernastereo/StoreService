e743b15f0a97d7d381e7eedb892103da299d2e0d
fernastereo <fernandoecueto@gmail.com>
2020-10-03 12:56:41 -0500

    creating dto classes for shop microservice

---
+++ ServiceStore.Api.Shop/Application/ShopDetailDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Shop.Application
+{
+    public class ShopDetailDto
+    {
+        public Guid? BookId { get; set; }
+        public string BookTitle { get; set; }
+        public string AuthorBook { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+    }
+}
---
+++ ServiceStore.Api.Shop/Application/ShopDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceStore.Api.Shop.Application
+{
+    public class ShopDto
+    {
+        public int ShopId { get; set; }
+        public DateTime? SessionCreatedAt { get; set; }
+        public List<ShopDetailDto> ProductList { get; set; }
+    }
+}
