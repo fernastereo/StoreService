2fb917d89424f4ca9fd20b18a8bb02247ae7d477
fernastereo <fernandoecueto@gmail.com>
2020-10-06 23:17:04 -0500

    implementing the unit test logic rules

--- ServiceStore.Api.Book.Tests/BookServiceTest.cs
+++ ServiceStore.Api.Book.Tests/BookServiceTest.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using Moq;
+using ServiceStore.Api.Book.Application;
+using ServiceStore.Api.Book.Persistence;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -10,7 +14,19 @@ namespace ServiceStore.Api.Book.Tests
         [Fact]
         public void GetBooks()
         {
+            //Which method of the microervice is in charge of query books?
 
+            //1. Emulate EFCore instance: BookContext
+            //To emulate the actions and events from an object in unit test environment que need to use a Mock object
+            //install: Moq package
+
+            var mockContext = new Mock<BookContext>();
+
+            //2. Emulate IMapper
+            var mockMapper = new Mock<IMapper>();
+
+            //3. Instanciate the Handler class and send as parameters the created mocks before
+            Query.Handler handler = new Query.Handler(mockContext.Object, mockMapper.Object);
         }
     }
 }
