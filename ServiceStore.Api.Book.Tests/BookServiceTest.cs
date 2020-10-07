4d1da371ce95600b8f24496c10fb29ae1413b19c
fernastereo <fernandoecueto@gmail.com>
2020-10-06 22:59:30 -0500

    Creating unit testing for book microservice

---
+++ ServiceStore.Api.Book.Tests/BookServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ServiceStore.Api.Book.Tests
+{
+    public class BookServiceTest
+    {
+        [Fact]
+        public void GetBooks()
+        {
+
+        }
+    }
+}
